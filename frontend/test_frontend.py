c44738455e1a16ef12f7597b73331acffccce2d1
lonely-caat <33058683+lonely-caat@users.noreply.github.com>
2019-11-13 18:14:18 +0200

    added comments to the frontend script

--- frontend/test_frontend.py
+++ frontend/test_frontend.py
@@ -10,22 +10,23 @@ xpaths = { 'search' : '//*[@name="search"]',
            'list_of_definitions' : "//ol",
            'submitButton' :   "//input[@name='login']"
          }
-
+## Fire it up! 
 mydriver = webdriver.Chrome()
 mydriver.get(baseurl)
 
-
+## First search input is ok
 mydriver.find_element_by_xpath(xpaths['search']).send_keys("apple")
 ## hit that 'Enter' button
 mydriver.find_element_by_xpath(xpaths['search']).send_keys(u'\ue007')
+## we have a HTML ordered list let's convert it to python list of strings 
 definitions = [x.text for x in mydriver.find_elements_by_xpath(xpaths['list_of_definitions'])]
 
+## Check if the expected string is a part of the ol
 assert apple_expected_string in definitions[0], "{0} not found on the page!".format(apple_expected_string)
 
-
+## go back to main page and repeat all the steps for the pear 
 mydriver.get(baseurl)
 mydriver.find_element_by_xpath(xpaths['search']).send_keys("pear")
-## hit that 'Enter' button
 mydriver.find_element_by_xpath(xpaths['search']).send_keys(u'\ue007')
 definitions = [x.text for x in mydriver.find_elements_by_xpath(xpaths['list_of_definitions'])]
 
